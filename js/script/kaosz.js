c258d4ba6893e43822bc06c758b7c318431beab4
Szántó Zsolt <zsozso121@gmail.com>
2015-08-28 02:39:00 +0200

    Pontelosztási opciók hozzáadása

--- js/script/kaosz.js
+++ js/script/kaosz.js
@@ -5,13 +5,100 @@
 var app = angular.module('kaoszApp', []);
 
 app.controller('kaoszCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.loading = true;
-    $scope.karakter = {
-      nev : "",
-      leiras : "",
-      faj : ""
-    };
-    readData($scope, $http)
+  $scope.loading = true;
+  
+  $scope.karakter = {
+    nev : "",
+    leiras : "",
+    foertekek : {},
+    pontosztas: "szabalyos",
+    egyenipontosztas: {
+      foertekpontok : 220,
+      jartassagpontok: 10000,
+      maxjartassagszint: 3
+    },
+    korosztaly: {
+      nev: "adultus",
+      juvenis: {
+	foertekpontok: 80,
+	jartassagpontok: 80
+      },
+      senius: {
+	foertekpontok: 80,
+	jartassagpontok: 200,
+	maxjartassagszint: 3
+      }
+    }
+  };
+  
+  $scope.consts = {
+    standard : "szabalyos",
+    custom : "egyeni",
+    juvenis : "juvenis",
+    adultus : "adultus",
+    senius : "senius",
+  };
+  
+  $scope.eloszthatoJartassagPontok = function() {
+    if ($scope.karakter.pontosztas == $scope.consts.custom){
+      return $scope.karakter.egyenipontosztas.jartassagpontok;
+    }
+    
+    if ($scope.konstansok === undefined) {
+      return 0;
+    }
+    var orig = $scope.konstansok.jartassagpontok._ertek;
+    
+    if ($scope.karakter.korosztaly.nev == $scope.consts.juvenis) {
+      return Math.round($scope.karakter.korosztaly.juvenis.jartassagpontok * orig / 100);
+    }
+    
+    if ($scope.karakter.korosztaly.nev == $scope.consts.senius) {
+      return Math.round($scope.karakter.korosztaly.senius.jartassagpontok * orig / 100);
+    }
+    
+    return orig;
+  };
+  
+  $scope.eloszthatoFoertekPontok = function() {
+    if ($scope.karakter.pontosztas == $scope.consts.custom){
+      return $scope.karakter.egyenipontosztas.foertekpontok;
+    }
+    
+    if ($scope.konstansok === undefined) {
+      return 0;
+    }
+    var orig = $scope.konstansok.foertekpontok._ertek;
+    
+    if ($scope.karakter.korosztaly.nev == $scope.consts.juvenis) {
+      return Math.round($scope.karakter.korosztaly.juvenis.foertekpontok * orig / 100);
+    }
+    
+    if ($scope.karakter.korosztaly.nev == $scope.consts.senius) {
+      return Math.round($scope.karakter.korosztaly.senius.foertekpontok * orig / 100);
+    }
+    
+    return orig;
+  };
+  
+  $scope.maxjartassagszint = function() {
+    if ($scope.karakter.pontosztas == $scope.consts.custom){
+      return $scope.karakter.egyenipontosztas.maxjartassagszint;
+    }
+    
+    if ($scope.karakter.korosztaly.nev == $scope.consts.senius) {
+      return $scope.karakter.korosztaly.senius.maxjartassagszint;
+    }
+    
+    if ($scope.fajok === undefined) {
+      return 0;
+    }
+    
+    return $scope.karakter.faj.maximalisJartassagszint;
+    
+  };
+  
+  readData($scope, $http);
 }]);
 
 function readData($scope, $http) {
@@ -37,4 +124,7 @@ function siteLoaded($scope, calls) {
   if (--calls.counter == 0) {
       $scope.loading = false;
   }
+  /*$scope.karakter.faj = "Ember";
+  $scope.karakter.eloszthatoFoertek: $scope.konstansok.foertekpontok._ertek;
+  $scope.karakter.eloszthatoJartassagpontok: $scope.konstansok.foertekpontok._ertek;*/
 }
