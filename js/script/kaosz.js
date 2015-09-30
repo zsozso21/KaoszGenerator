3f8c3b414859685d4406af45207fa45909491a0d
Szántó Zsolt <zsozso121@gmail.com>
2015-10-01 01:31:23 +0200

    observation test

--- js/script/kaosz.js
+++ js/script/kaosz.js
@@ -105,6 +105,80 @@ app.controller('kaoszCtrl', ['$scope', '$http', function($scope, $http) {
   readData($scope, $http);
 }]);
 
+app.directive('spin', function() {
+    return {
+        templateUrl: 'script/spinner.html',
+        scope: {
+          'value' : "="
+        },
+        restrict: 'E',
+        link: function(scope, element, attrs, ngModel) {
+          var min,max,step,value,input,initial;
+
+          element = angular.element(element);
+
+          if(typeof attrs === 'undefined'){
+            throw new Error('Spin.js attributes missing');
+          } else {
+            min = typeof attrs.min !== 'undefined' ? attrs.min : 0;
+            max = typeof attrs.max !== 'undefined' ? attrs.max : 999;
+            step = typeof attrs.step !== 'undefined' ? attrs.step : 1;
+            
+            initial = parseInt(scope.value);
+
+            input = $("input[name='spin']",element);
+            input.TouchSpin({
+                min: min,
+                max: max,
+                step: step,
+                initval: initial,
+                forcestepdivisibility : 'none',
+                booster : false
+            });
+	    
+	    attrs.$observe('min', function(value) { 
+	      input.TouchSpin({
+                min: value,
+	      });
+	      //alert(input);
+	      //alert('d1-obsrv a6:' + value);
+	    });
+// 	    attrs.$observe('min', ...){
+// 	      alert("dfadf");
+// 	    }
+
+            input.on('change', function(e){
+              scope.value = input.val();
+              //hack
+              if(!scope.$$phase) {
+                scope.$apply();
+              }
+
+            });
+
+          }
+        }/*,
+        compile: function(tElement, tAttrs) {
+	  return function link(scope, iElement, iAttrs) {
+	    iAttrs.$observe('min', function(value) { alert('d1-obsrv a6:' + value); });
+// 	    console.log('d1-link:', iAttrs.attr1, iAttrs.attr2, iAttrs.attr3, iAttrs.attr4, iAttrs.attr5, tAttrs.attr6);
+// 	    scope.$watch(iAttrs.attr1, function(value) { console.log('d1-watch a1:', value); });
+// 	    scope.$watch(iAttrs.attr2, function(value) { console.log('d1-watch a2:', value); });
+// 	    scope.$watch(iAttrs.attr3, function(value) { console.log('d1-watch a3:', value); });
+// 	    scope.$watch(iAttrs.attr4, function(value) { console.log('d1-watch a4:', value); });
+// 	    scope.$watch(iAttrs.attr5, function(value) { console.log('d1-watch a5:', value); });
+// 	    scope.$watch(iAttrs.attr6, function(value) { console.log('d1-watch a6:', value); });
+// 	    iAttrs.$observe('attr1', function(value) { console.log('d1-obsrv a1:', value); });
+// 	    iAttrs.$observe('attr2', function(value) { console.log('d1-obsrv a2:', value); });
+// 	    iAttrs.$observe('attr3', function(value) { console.log('d1-obsrv a3:', value); });
+// 	    iAttrs.$observe('attr4', function(value) { console.log('d1-obsrv a4:', value); });
+// 	    iAttrs.$observe('attr5', function(value) { console.log('d1-obsrv a5:', value); });
+// 	    iAttrs.$observe('attr6', function(value) { console.log('d1-obsrv a6:', value); });
+	  };
+	}*/
+    };
+}); 
+
 function readData($scope, $http) {
   var calls = {counter : 3};
   readXML($scope, $http, 'data/xml/konstansok.xml', calls);
